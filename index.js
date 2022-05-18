af1335ec62d0f1cfe1995f4a085f054c9f20cf0c
absurd-turtle <absurd.turtl3@gmail.com>
2022-05-18 19:45:30 +0300

    implemented 3.3

--- index.js
+++ index.js
@@ -39,6 +39,18 @@ app.get('/api/info', (request, response) => {
 `)
 })
 
+
+app.get('/api/person/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  }
+  else {
+    response.status(404).end()
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
