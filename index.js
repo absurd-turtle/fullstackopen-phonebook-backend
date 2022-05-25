aefd039ff88fee9be850ef6d839b63e46a40cf49
absurd-turtle <absurd.turtl3@gmail.com>
2022-05-25 11:00:17 +0300

    implemented 3.15-3.16

--- index.js
+++ index.js
@@ -32,13 +32,24 @@ app.get('/api/info', (request, response) => {
   })
 })
 
-app.get('/api/person/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
+app.get('/api/person/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/person/:id', (request, response) => {
+app.delete('/api/person/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/person', (request, response) => {
@@ -58,9 +69,29 @@ app.post('/api/person', (request, response) => {
     return response.json(savedPerson)
   })
 })
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
